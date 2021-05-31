7347df8184193cc50eb8197f043efd75f6ac9910
daomingzhu <596167028@qq.com>
2021-05-31 16:51:33 +0800

    【bug修复】网关项目初始化

---
+++ qk-dm-micro-services/gateway/src/main/java/com/qk/dm/dmgateway/DmGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.qk.dm.dmgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DmGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DmGatewayApplication.class, args);
+    }
+
+}
---
+++ qk-dm-micro-services/gateway/src/test/java/com/qk/dm/dmgateway/DmGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qk.dm.dmgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DmGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
