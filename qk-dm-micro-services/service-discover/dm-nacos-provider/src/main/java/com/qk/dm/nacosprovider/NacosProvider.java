bdd945492dd5c84685a27b7db3a968094ac42919
daomingzhu <596167028@qq.com>
2021-05-31 18:55:05 +0800

    【服务注册】整合nacos 【配置中心】整合nacos 【网关】基础spring cloud gateway

--- qk-dm-micro-services/service-discover/dm-nacos-provider/src/main/java/com/qk/dm/nacosprovider/NacosProvider.java
+++ qk-dm-micro-services/service-discover/dm-nacos-provider/src/main/java/com/qk/dm/nacosprovider/NacosProvider.java
@@ -1,17 +1,13 @@
-package com.qk.dm.dmgateway;
+package com.qk.dm.nacosprovider;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-@EnableFeignClients
-public class DmGatewayApplication {
-
+public class NacosProvider {
     public static void main(String[] args) {
-        SpringApplication.run(DmGatewayApplication.class, args);
+        SpringApplication.run(NacosProvider.class, args);
     }
-
 }
