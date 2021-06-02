cb9f0c48a73e58abb3a2e39fc259e71647a84921
daomingzhu <596167028@qq.com>
2021-06-02 18:00:01 +0800

    【多数据源支持】支持动态替换 【querydsl+spring boot jpa】持久层框架搭建完毕

--- common-lib/dam-datasource/src/main/java/com/qk/datasource/config/DatasourceConfiguration.java
+++ common-lib/dam-datasource/src/main/java/com/qk/datasource/config/DatasourceConfiguration.java
@@ -1,22 +1,39 @@
 package com.qk.datasource.config;
 
 import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
-
 import javax.sql.DataSource;
 
+/**
+ * @author daomingzhu
+ * @date 2021/06/02
+ * 多数据源配置类，支持cloud config 动态刷新
+ */
 @Configuration
 public class DatasourceConfiguration {
+    @Bean
+    @ConfigurationProperties(prefix = "spring.datasource",ignoreInvalidFields=true)
+    public DatasourceActive datasourceActive(){
+        return new DatasourceActive();
+    }
 
     @Bean
-    @Primary
+    @ConfigurationProperties(prefix = "spring.datasource.mysql",ignoreInvalidFields=true)
     public DataSource mysqlDataSource(){
         return new HikariDataSource();
     }
     @Bean
+    @ConfigurationProperties(prefix = "spring.datasource.oracle",ignoreInvalidFields=true)
+    public DataSource oracleSqlSource(){
+        return new HikariDataSource();
+    }
+    @Bean
+    @ConfigurationProperties(prefix = "spring.datasource.postgresql",ignoreInvalidFields=true)
     public DataSource postgreSqlSource(){
         return new HikariDataSource();
     }
+
 }
--- qk-dm-boots/qk-dm-micro-services/gateway/src/main/java/com/qk/dm/dmgateway/DmGatewayApplication.java
+++ qk-dm-boots/qk-dm-micro-services/gateway/src/main/java/com/qk/dm/dmgateway/DmGatewayApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.ComponentScan;
 
 /**
  * 统一接入网关
@@ -12,6 +13,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableFeignClients
+@ComponentScan(basePackages = "com.qk")
 public class DmGatewayApplication {
 
     public static void main(String[] args) {
