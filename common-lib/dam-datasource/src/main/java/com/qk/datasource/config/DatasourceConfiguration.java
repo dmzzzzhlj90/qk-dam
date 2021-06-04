83e50e69c61065c09ffec650c1059aae27151728
daomingzhu <596167028@qq.com>
2021-06-04 16:08:19 +0800

    【持久化框架】ConfigurationProperties 依据配置选择 bean

--- common-lib/dam-datasource/src/main/java/com/qk/datasource/config/DatasourceConfiguration.java
+++ common-lib/dam-datasource/src/main/java/com/qk/datasource/config/DatasourceConfiguration.java
@@ -1,6 +1,7 @@
 package com.qk.datasource.config;
 
 import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -20,16 +21,21 @@ public class DatasourceConfiguration {
     }
 
     @Bean
+    @ConditionalOnProperty(prefix = "spring.datasource",name = "active",havingValue = "mysql")
     @ConfigurationProperties(prefix = "spring.datasource.mysql",ignoreInvalidFields=true)
     public DataSource mysqlDataSource(){
         return new HikariDataSource();
     }
+
     @Bean
+    @ConditionalOnProperty(prefix = "spring.datasource",name = "active",havingValue = "oracle")
     @ConfigurationProperties(prefix = "spring.datasource.oracle",ignoreInvalidFields=true)
     public DataSource oracleSqlSource(){
         return new HikariDataSource();
     }
+
     @Bean
+    @ConditionalOnProperty(prefix = "spring.datasource",name = "active",havingValue = "postgre")
     @ConfigurationProperties(prefix = "spring.datasource.postgresql",ignoreInvalidFields=true)
     public DataSource postgreSqlSource(){
         return new HikariDataSource();
