1c078d7b9fcb3b42014da9d1940fbbb5e284af6d
daomingzhu <596167028@qq.com>
2021-06-08 16:26:38 +0800

    【datax】配置文件

--- qk-dam/qike-data-pici/src/main/java/com/qk/dm/dataipici/EngineTest.java
+++ qk-dam/qike-data-pici/src/main/java/com/qk/dm/dataipici/EngineTest.java
@@ -1,10 +1,10 @@
-package com.qk.dm.dataingestion;
+package com.qk.dm.dataipici;
 
 import com.alibaba.datax.core.Engine;
 
 public class EngineTest {
     public static void main(String[] args) {
-        System.setProperty("datax.home", "/Users/daomingzhu/DataX/core/target/datax");
+        System.setProperty("datax.home", "/Users/daomingzhu/Documents/datax");
         String[] datxArgs = {"-job", "/Users/daomingzhu/qk-dam/qk-dam/qike-dm-dataingestion/src/main/resources/stream2stream.json", "-mode", "standalone", "-jobid", "-1"};
         try {
             //从这里启动
