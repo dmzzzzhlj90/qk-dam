241c52b31e6a0cda5f60c0bd5346d4b95eb422b3
weijunqi <weijunqi@qike366.com>
2021-07-06 16:12:16 +0800

    [消息监控服务] 修改同步prometheus实时value,增加监控调度服务状态指标;

--- qk-dm-boots/qk-dm-micro-services/actuator-mxserver/src/main/java/com/qk/dm/controller/MxTaskSchedulerController.java
+++ qk-dm-boots/qk-dm-micro-services/actuator-mxserver/src/main/java/com/qk/dm/controller/MxTaskSchedulerController.java
@@ -7,6 +7,7 @@ import com.qk.dam.commons.http.result.DefaultCommonResult;
 import com.qk.dm.service.MxTaskSchedulerService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -22,20 +23,24 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/task/scheduler")
 public class MxTaskSchedulerController {
-  private static final Log LOG = LogFactory.get("监控调度任务实例状态情况");
+    private static final Log LOG = LogFactory.get("监控调度任务实例状态情况");
 
-  private final MxTaskSchedulerService mxTaskSchedulerService;
+    @Value("${scheduler.timer.param}")
+    private String timerParam;
 
-  @Autowired
-  public MxTaskSchedulerController(MxTaskSchedulerService mxTaskSchedulerService) {
-    this.mxTaskSchedulerService = mxTaskSchedulerService;
-  }
+    private final MxTaskSchedulerService mxTaskSchedulerService;
 
-  @GetMapping("/process/instance/state")
-  public DefaultCommonResult sendProcessInstanceState() {
-    LOG.info("定时监控开始!");
-    //        LOG.info("定时器时间cron为:【{}】!", timerParam);
-    mxTaskSchedulerService.sendProcessInstanceState();
-    return new DefaultCommonResult(ResultCodeEnum.OK);
-  }
+    @Autowired
+    public MxTaskSchedulerController(MxTaskSchedulerService mxTaskSchedulerService) {
+        this.mxTaskSchedulerService = mxTaskSchedulerService;
+    }
+
+
+    @GetMapping("/process/instance/state")
+    public DefaultCommonResult sendProcessInstanceState() {
+        LOG.info("定时监控开始!");
+        LOG.info("定时器时间cron为:【{}】!", timerParam);
+        mxTaskSchedulerService.sendProcessInstanceState();
+        return new DefaultCommonResult(ResultCodeEnum.OK);
+    }
 }
--- qk-dm-boots/qk-dm-micro-services/actuator-mxserver/src/main/java/com/qk/dm/service/impl/MxTaskSchedulerServiceImpl.java
+++ qk-dm-boots/qk-dm-micro-services/actuator-mxserver/src/main/java/com/qk/dm/service/impl/MxTaskSchedulerServiceImpl.java
@@ -9,12 +9,14 @@ import com.qk.dm.constant.CustomMetricsConstant;
 import com.qk.dm.service.MxTaskSchedulerService;
 import io.micrometer.core.instrument.Gauge;
 import io.micrometer.core.instrument.MeterRegistry;
-import java.text.SimpleDateFormat;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
-import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
 
 /**
  * @author wjq
@@ -23,52 +25,56 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class MxTaskSchedulerServiceImpl implements MxTaskSchedulerService {
+    @Value("${dolphin_scheduler.user_names}")
+    private String schedulerUserName;
+    @Value("${dolphin_scheduler.prevHour}")
+    private String schedulerPrevHour;
 
-  private static final Log LOG = LogFactory.get("监控调度任务实例状态情况");
-  private final MeterRegistry meterRegistry;
+    private static final Log LOG = LogFactory.get("监控调度任务实例状态情况");
+    private final MeterRegistry meterRegistry;
 
-  @Autowired
-  public MxTaskSchedulerServiceImpl(MeterRegistry meterRegistry) {
-    this.meterRegistry = meterRegistry;
-  }
+    @Autowired
+    public MxTaskSchedulerServiceImpl(MeterRegistry meterRegistry) {
+        this.meterRegistry = meterRegistry;
+    }
 
-  @Override
-  public void sendProcessInstanceState() {
-    String dataDay =
-        DateTimeFormatter.ofPattern(LongGovConstant.DATE_TIME_PATTERN).format(LocalDateTime.now());
-    LOG.info("开始查询当日【{}】调度任务实例数据数据", dataDay);
-    SimpleDateFormat simpleDateFormat =
-        new SimpleDateFormat(LongGovConstant.DATE_TIME_SECOND_PATTERN);
+    @Override
+    public void sendProcessInstanceState() {
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(Calendar.HOUR, (calendar.get(Calendar.HOUR) - Integer.parseInt(schedulerPrevHour)));
+        SimpleDateFormat df = new SimpleDateFormat(LongGovConstant.DATE_TIME_SECOND_PATTERN);
+        String prevHourDate = df.format(calendar.getTime());
+        String nowDate = df.format(new Date());
 
-    List<DolphinProcessInstanceVO> processInstanceVOList =
-        DolphinSchedulerAgg.schedulerProcessInstanceInfo(dataDay);
-    LOG.info("查询到调度任务实例数据数量为:【{}】!", processInstanceVOList.size());
+        LOG.info("开始查询调度任务实例数据数据,开始时间为:【{}】,结束时间为:【{}】", prevHourDate, nowDate);
+        List<DolphinProcessInstanceVO> processInstanceVOList = DolphinSchedulerAgg.schedulerProcessInstanceInfo(prevHourDate, nowDate, schedulerUserName);
+        LOG.info("查询到调度任务实例数据数量为:【{}】!", processInstanceVOList.size());
 
-    LOG.info("开始同步调度任务实例数据到prometheus中......");
-    processInstanceVOList.forEach(
-        DPIV -> {
-          int state = DPIV.getState();
-          Gauge.builder(
-                  CustomMetricsConstant.CUSTOM_TASK_SCHEDULER_PROCESS_INSTANCE,
-                  state,
-                  Integer::intValue)
-              .tags(
-                  CustomMetricsConstant.TASK_SCHEDULER_PROCESS_DEFINITION_CODE,
-                  String.valueOf(DPIV.getProcess_definition_code()),
-                  CustomMetricsConstant.TASK_SCHEDULER_NAME,
-                  DPIV.getName(),
-                  CustomMetricsConstant.TASK_SCHEDULER_STATE,
-                  String.valueOf(DPIV.getState()),
-                  CustomMetricsConstant.TASK_SCHEDULER_USER_NAME,
-                  DPIV.getUser_name(),
-                  CustomMetricsConstant.TASK_SCHEDULER_UPDATE_TIME,
-                  DPIV.getUpdate_time())
-              .register(meterRegistry);
-          LOG.info(
-              "同步任务实例ID为【{}】,名称为:【{}】的数据到prometheus中......",
-              DPIV.getProcess_definition_code(),
-              DPIV.getName());
-        });
-    LOG.info("成功同步调度任务实例数据到prometheus中!!!");
-  }
+        LOG.info("开始同步调度任务实例数据到prometheus中......");
+        meterRegistry.clear();
+        processInstanceVOList.forEach(
+                DPIV -> {
+                    int state = DPIV.getState();
+                    Gauge.builder(CustomMetricsConstant.CUSTOM_TASK_SCHEDULER_PROCESS_INSTANCE, state, Integer::intValue)
+                            .tags(
+                                    CustomMetricsConstant.TASK_SCHEDULER_PROCESS_DEFINITION_CODE,
+                                    String.valueOf(DPIV.getProcess_definition_code()),
+                                    CustomMetricsConstant.TASK_SCHEDULER_NAME,
+                                    DPIV.getName(),
+                                    CustomMetricsConstant.TASK_SCHEDULER_STATE,
+                                    String.valueOf(DPIV.getState()),
+                                    CustomMetricsConstant.TASK_SCHEDULER_USER_NAME,
+                                    DPIV.getUser_name(),
+                                    CustomMetricsConstant.TASK_SCHEDULER_UPDATE_TIME,
+                                    DPIV.getUpdate_time()
+                            )
+                            .register(meterRegistry);
+                    LOG.info(
+                            "同步任务实例ID为【{}】,名称为:【{}】的数据到prometheus中......",
+                            DPIV.getProcess_definition_code(),
+                            DPIV.getName());
+                });
+        LOG.info("成功同步调度任务实例数据到prometheus中!!!");
+    }
+
 }
