b69bcbe0ed87bafcfc23c98890d77900d917d7ef
wangzhipan <zhipan234@163.com>
2021-11-17 15:15:41 +0800

    【数据模型服务】添加维度表接口

---
+++ qk-dam/qike-dm-datamodel/src/main/java/com/qk/dm/datamodel/service/ModelDimTableColumnService.java
@@ -0,0 +1,18 @@
+package com.qk.dm.datamodel.service;
+
+import com.qk.dm.datamodel.params.dto.ModelDimTableColumnDTO;
+import com.qk.dm.datamodel.params.vo.ModelDimTableColumnVO;
+
+import java.util.List;
+
+public interface ModelDimTableColumnService {
+    void insert(List<ModelDimTableColumnDTO> modelDimTableColumnDTOList);
+
+    ModelDimTableColumnVO detail(Long id);
+
+    void update(Long dimTableId,List<ModelDimTableColumnDTO> modelDimTableColumnDTOList);
+
+    void delete(String ids);
+
+    List<ModelDimTableColumnVO> list(Long dimTableId);
+}
