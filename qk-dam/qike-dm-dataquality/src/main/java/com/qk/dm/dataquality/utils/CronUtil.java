20d81a7159293963f0588aa086e6cd071712ca9b
shenpengjie <286777199@qq.com>
2021-12-06 11:48:13 +0800

    [数据质量] cron周生成调整

--- qk-dam/qike-dm-dataquality/src/main/java/com/qk/dm/dataquality/utils/CronUtil.java
+++ qk-dam/qike-dm-dataquality/src/main/java/com/qk/dm/dataquality/utils/CronUtil.java
@@ -12,87 +12,80 @@ import com.qk.dm.dataquality.vo.DqcSchedulerConfigVO;
  * @since 1.0.0
  */
 public class CronUtil {
-  /**
-   * 方法摘要：构建Cron表达式
-   *
-   * @param taskScheduleModel
-   * @return String
-   */
-  public static String createCron(DqcSchedulerConfigVO taskScheduleModel) {
-    return createCron(
-        taskScheduleModel.getSchedulerCycle(),
-        taskScheduleModel.getSchedulerIntervalTime(),
-        taskScheduleModel.getSchedulerTime());
-  }
-
-  public static String createCron(String type, String interval, String time) {
-    StringBuffer cronExp = new StringBuffer("");
-
-    if (null == type) {
-      // 执行周期未配置
-      throw new BizException("执行周期未配置");
+    /**
+     * 方法摘要：构建Cron表达式
+     *
+     * @param taskScheduleModel
+     * @return String
+     */
+    public static String createCron(DqcSchedulerConfigVO taskScheduleModel) {
+        return createCron(taskScheduleModel.getSchedulerCycle(), taskScheduleModel.getSchedulerIntervalTime(), taskScheduleModel.getSchedulerTime());
     }
-    switch (SchedulerCycleEnum.fromValue(type)) {
-      case minute:
-        minute(interval, cronExp);
-        break;
-      case hour:
-        hour(interval, cronExp);
-        break;
-      case day:
-        day(time, cronExp);
-        break;
-      case week:
-        weeks(interval, time, cronExp);
-        break;
+
+    public static String createCron(String type, String interval, String time) {
+        StringBuffer cronExp = new StringBuffer("");
+
+        if (null == type) {
+            // 执行周期未配置
+            throw new BizException("执行周期未配置");
+        }
+        switch (SchedulerCycleEnum.fromValue(type)) {
+            case minute:
+                minute(interval, cronExp);
+                break;
+            case hour:
+                hour(interval, cronExp);
+                break;
+            case day:
+                day(time, cronExp);
+                break;
+            case week:
+                weeks(interval, time, cronExp);
+                break;
+        }
+        return cronExp.toString();
     }
-    return cronExp.toString();
-  }
 
-  private static void minute(String interval, StringBuffer cronExp) {
-    cronExp.append("0 "); // 秒
-    cronExp.append("0/").append(interval).append(" ");
-    cronExp.append("* "); // 时
-    cronExp.append("* "); // 日
-    cronExp.append("* "); // 月
-    cronExp.append("?"); // 周
-  }
+    private static void minute(String interval, StringBuffer cronExp) {
+        cronExp.append("0 "); // 秒
+        cronExp.append("0/").append(interval).append(" ");
+        cronExp.append("* "); // 时
+        cronExp.append("* "); // 日
+        cronExp.append("* "); // 月
+        cronExp.append("?"); // 周
+    }
 
-  private static void hour(String interval, StringBuffer cronExp) {
-    cronExp.append("0 ");
-    cronExp.append("0 ");
-    cronExp.append("0/").append(interval).append(" ");
-    cronExp.append("* ");
-    cronExp.append("* ");
-    cronExp.append("?");
-  }
+    private static void hour(String interval, StringBuffer cronExp) {
+        cronExp.append("0 ");
+        cronExp.append("0 ");
+        cronExp.append("0/").append(interval).append(" ");
+        cronExp.append("* ");
+        cronExp.append("* ");
+        cronExp.append("?");
+    }
 
-  private static void day(String time, StringBuffer cronExp) {
-    common(time, cronExp);
-    cronExp.append("?");
-  }
+    private static void day(String time, StringBuffer cronExp) {
+        DateTime parse = DateUtil.parse(time, "HH:mm");
+        cronExp.append("0 ");
+        cronExp.append(DateUtil.minute(parse)).append(" ");
+        cronExp.append(DateUtil.hour(parse, true)).append(" ");
+        cronExp.append("* ");
+        cronExp.append("* ");
+        cronExp.append("?");
+    }
 
-  private static void weeks(String interval, String time, StringBuffer cronExp) {
-    common(time, cronExp);
-    cronExp.append(interval);
-  }
-
-  private static void common(String time, StringBuffer cronExp) {
-    DateTime parse = DateUtil.parse(time, "HH:mm");
-    cronExp.append("0 ");
-    cronExp.append(DateUtil.minute(parse)).append(" ");
-    cronExp.append(DateUtil.hour(parse, true)).append(" ");
-    cronExp.append("* ");
-    cronExp.append("* ");
-  }
-
-  // 参考例子
-  public static void main(String[] args) {
-    // 执行时间：每天的12时12分12秒 start
-    DqcSchedulerConfigVO dqcSchedulerConfigVO = new DqcSchedulerConfigVO();
-    dqcSchedulerConfigVO.setSchedulerCycle("day");
-    dqcSchedulerConfigVO.setSchedulerIntervalTime("1");
-    dqcSchedulerConfigVO.setSchedulerTime("01:05");
-    System.out.println(createCron(dqcSchedulerConfigVO));
-  }
+    private static void weeks(String interval, String time, StringBuffer cronExp) {
+        //MON TUE WED THU FRI SAT SUN
+        DateTime parse = DateUtil.parse(time, "HH:mm");
+        cronExp.append("0 ");
+        cronExp.append(DateUtil.minute(parse)).append(" ");
+        cronExp.append(DateUtil.hour(parse, true)).append(" ");
+        cronExp.append("? ");
+        cronExp.append("* ");
+        if (interval.split(",").length == 7) {
+            cronExp.append("* ");
+        } else {
+            cronExp.append(interval);
+        }
+    }
 }
