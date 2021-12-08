c23e89c77ba7e686230642a17f987dab0d7fc4c8
zhuyunsan <zhu19930306>
2021-12-08 14:41:12 +0800

    数据采集基础类搭建

---
+++ qk-reptile/qk-data-reptile/src/main/java/com/qk/dm/reptile/service/impl/RptDimensionInfoServerImpl.java
@@ -0,0 +1,11 @@
+package com.qk.dm.reptile.service.impl;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * @author zys
+ * @date 2021/12/8 14:39
+ * @since 1.0.0
+ */@Service
+public class RptDimensionInfoServerImpl {
+}
