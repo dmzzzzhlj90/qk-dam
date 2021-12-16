fc3b6d4da84a7eea54dc5a5ecd7fd5634a5e0e5a
wangzhipan <zhipan234@163.com>
2021-12-16 11:46:41 +0800

    [元数据服务]修改获取数据库接口

--- common-lib/dam-metadata-common/src/main/java/com/qk/dam/metedata/vo/MtdTableSearchVO.java
+++ common-lib/dam-metadata-common/src/main/java/com/qk/dam/metedata/vo/MtdTableSearchVO.java
@@ -13,17 +13,15 @@ import javax.validation.constraints.NotNull;
 public class MtdTableSearchVO extends MtdDbSearchVO{
     public MtdTableSearchVO(){}
     public MtdTableSearchVO(String typeName, String dbName, String server) {
-        super(typeName, dbName);
-        this.server = server;
+        super(typeName, server);
+        this.dbName = dbName;
     }
 
     public MtdTableSearchVO(Integer limit, Integer offset, String typeName, String dbName, String server) {
-        super(limit, offset, typeName, dbName);
-        this.server = server;
+        super(limit, offset, typeName, server);
+        this.dbName = dbName;
     }
     @NotNull(message = "dbName不能为空！")
     private String dbName;
-    @NotNull(message = "元数据数据库地址不能为空！")
-    protected String server;
 
 }
