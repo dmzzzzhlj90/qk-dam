257fd7d911921dac1b4fe2c648b8401660cd3c69
zhuyunsan <zhu19930306>
2021-12-23 14:34:10 +0800

    可视化数据采集-导入excel

--- qk-reptile/qk-data-reptile/src/main/java/com/qk/dm/reptile/service/impl/RptExcelBatchService.java
+++ qk-reptile/qk-data-reptile/src/main/java/com/qk/dm/reptile/service/impl/RptExcelBatchService.java
@@ -1,6 +1,8 @@
 package com.qk.dm.reptile.service.impl;
 
+import com.alibaba.nacos.common.utils.CollectionUtils;
 import com.qk.dm.reptile.entity.RptBaseInfo;
+import com.qk.dm.reptile.repositories.RptBaseInfoRepository;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.stereotype.Service;
@@ -8,7 +10,8 @@ import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
 
 /**
  * @author zys
@@ -20,9 +23,15 @@ public class RptExcelBatchService {
   private static final Log LOG = LogFactory.getLog("rptExcelBatchService");
   @PersistenceContext
   private EntityManager entityManager;
+  private final RptBaseInfoRepository rptBaseInfoRepository;
+
+  public RptExcelBatchService(RptBaseInfoRepository rptBaseInfoRepository) {
+    this.rptBaseInfoRepository = rptBaseInfoRepository;
+  }
+
   @Transactional(rollbackFor = Exception.class)
   public void saveRptBasicInfo(List<RptBaseInfo> prtBasicInfoList) {
-
+    deal(prtBasicInfoList);
     for (RptBaseInfo rptBaseInfo : prtBasicInfoList) {
       //todo 加入操作人员id
       entityManager.persist(rptBaseInfo); // insert插入操作
@@ -30,5 +39,25 @@ public class RptExcelBatchService {
     entityManager.flush();
     entityManager.clear();
     LOG.info(prtBasicInfoList.size()+"成功保存待配信息个数 【{}】");
+  }
+
+  private void deal(List<RptBaseInfo> prtBasicInfoList) {
+    if (CollectionUtils.isNotEmpty(prtBasicInfoList)){
+      List<RptBaseInfo> allList = rptBaseInfoRepository.findAll();
+      if (CollectionUtils.isNotEmpty(allList)){
+        Map<String,RptBaseInfo> collect = allList.stream().collect(Collectors
+            .toMap(k -> k.getWebsiteUrl() + "->"+k.getSecondSiteType()+"->" + k.getListPageAddress(),
+                k -> k, (k1, k2) -> k1));
+        Iterator<RptBaseInfo> iterator = prtBasicInfoList.iterator();
+        while (iterator.hasNext()){
+          RptBaseInfo rptBaseInfo = iterator.next();
+          String s = rptBaseInfo.getWebsiteUrl() +"->"+rptBaseInfo.getSecondSiteType()+"->"+ rptBaseInfo.getListPageAddress();
+          RptBaseInfo rptBaseInfo1 = collect.get(s);
+          if (!Objects.isNull(rptBaseInfo1)){
+            iterator.remove();
+          }
+        }
+      }
+    }
   }
 }
