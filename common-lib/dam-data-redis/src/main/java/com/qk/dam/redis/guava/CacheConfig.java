f9ac41f0d9296941ae71b80b945ee80e40d3429a
shenpengjie <286777199@qq.com>
2022-01-18 10:18:39 +0800

    [数据质量] redis缓存更改为本地缓存

--- common-lib/dam-data-redis/src/main/java/com/qk/dam/redis/guava/CacheConfig.java
+++ common-lib/dam-data-redis/src/main/java/com/qk/dam/redis/guava/CacheConfig.java
@@ -20,7 +20,7 @@ public class CacheConfig {
         CaffeineCacheManager cacheManager = new CaffeineCacheManager();
         cacheManager.setCaffeine(Caffeine.newBuilder()
                 // 设置最后一次写入或访问后经过固定时间过期
-                .expireAfterAccess(5, TimeUnit.SECONDS)
+                .expireAfterAccess(4, TimeUnit.MINUTES)
                 // 初始的缓存空间大小
                 .initialCapacity(100)
                 // 缓存的最大条数
