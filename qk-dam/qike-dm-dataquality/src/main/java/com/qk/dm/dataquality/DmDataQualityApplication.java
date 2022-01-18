3d4d46d998f649fb318cfbb10fc430c18140e805
zhudaoming <daomingzhu@icloud.com>
2022-01-18 12:06:06 +0800

    【数据质量】指标埋点，dockerfile支持上海时区

--- qk-dam/qike-dm-dataquality/src/main/java/com/qk/dm/dataquality/DmDataQualityApplication.java
+++ qk-dam/qike-dm-dataquality/src/main/java/com/qk/dm/dataquality/DmDataQualityApplication.java
@@ -6,6 +6,7 @@ import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 /**
@@ -17,6 +18,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableDiscoveryClient
 @EnableFeignClients(basePackages = "com.qk.dm.*")
 @ComponentScan(basePackages = "com.qk")
+@EnableAspectJAutoProxy
 @EnableScheduling
 @EnableCaching
 public class DmDataQualityApplication {
