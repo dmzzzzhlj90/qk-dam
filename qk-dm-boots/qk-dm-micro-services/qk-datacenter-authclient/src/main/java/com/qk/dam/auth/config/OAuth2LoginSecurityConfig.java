d11e10a4dac1cfa2e973ef683890ceddc0d977a1
zhudaoming <daomingzhu@icloud.com>
2022-01-24 18:31:56 +0800

    merge dev

---
+++ qk-dm-boots/qk-dm-micro-services/qk-datacenter-authclient/src/main/java/com/qk/dam/auth/config/OAuth2LoginSecurityConfig.java
@@ -0,0 +1,20 @@
+package com.qk.dam.auth.config;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@EnableWebSecurity
+public class OAuth2LoginSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+            .authorizeRequests(authorize -> authorize
+                .anyRequest().authenticated()
+            )
+            .oauth2Login(withDefaults());
+    }
+}
