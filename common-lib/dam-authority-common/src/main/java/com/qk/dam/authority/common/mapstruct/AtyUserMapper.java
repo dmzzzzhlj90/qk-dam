718dc664fea760037fd7d3518ba669b36f4b1f50
shenpengjie <286777199@qq.com>
2022-03-11 12:10:17 +0800

    [权限管理] 提取配置到配置中心

--- common-lib/dam-authority-common/src/main/java/com/qk/dam/authority/common/mapstruct/AtyUserMapper.java
+++ common-lib/dam-authority-common/src/main/java/com/qk/dam/authority/common/mapstruct/AtyUserMapper.java
@@ -4,9 +4,14 @@ import com.qk.dam.authority.common.vo.user.AtyUserInfoVO;
 import com.qk.dam.authority.common.vo.user.AtyUserInputExceVO;
 import com.qk.dam.authority.common.vo.user.AtyUserKeyCloakVO;
 import org.keycloak.representations.idm.UserRepresentation;
-import org.mapstruct.*;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.NullValueCheckStrategy;
+import org.mapstruct.NullValuePropertyMappingStrategy;
 import org.mapstruct.factory.Mappers;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 
 /**
@@ -19,13 +24,26 @@ import java.util.List;
 public interface AtyUserMapper {
     AtyUserMapper INSTANCE = Mappers.getMapper(AtyUserMapper.class);
 
-    @Mapping(target = "createdTimestamp", expression = "java(com.qk.dam.authority.common.utils.TimeUtil.parseLong(userRepresentation.getCreatedTimestamp()))")
+    @Mapping(target = "createdTimestamp", expression = "java(com.qk.dam.authority.common.mapstruct.AtyUserMapper.parseLong(userRepresentation.getCreatedTimestamp()))")
     AtyUserInfoVO userInfo(UserRepresentation userRepresentation);
 
-    @Mapping(target = "createdTimestamp", expression = "java(com.qk.dam.authority.common.utils.TimeUtil.parseLong(userRepresentation.getCreatedTimestamp()))")
+    @Mapping(target = "createdTimestamp", expression = "java(com.qk.dam.authority.common.mapstruct.AtyUserMapper.parseLong(userRepresentation.getCreatedTimestamp()))")
     List<AtyUserInfoVO> userInfo(List<UserRepresentation> userRepresentation);
 
     UserRepresentation userInfo(AtyUserKeyCloakVO userVO);
 
     UserRepresentation userExcelInfo(AtyUserInputExceVO atyUserInputExceVO);
+
+    /**
+     * 根据格式，Long 转 string
+     */
+    static String parseLong(Long timestamp) {
+        Date date = new Date(timestamp);
+        SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        try {
+            return df.format(date);
+        } catch (Exception e) {
+            return null;
+        }
+    }
 }
