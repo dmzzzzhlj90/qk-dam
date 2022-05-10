ccbf09e948fed65fdeb6f623532370a94d1d8c33
dmzzzzhlj90 <102905603+dmzzzzhlj90@users.noreply.github.com>
2022-05-10 17:21:13 +0800

    fix: dsd 升级jdk为oracle版本

--- starter/dam-boot-cache-starter/dam-cache-common/src/main/java/com/qk/dam/cache/CacheManagerEnum.java
+++ starter/dam-boot-cache-starter/dam-cache-common/src/main/java/com/qk/dam/cache/CacheManagerEnum.java
@@ -13,17 +13,6 @@ public enum CacheManagerEnum {
   TEST("test", "", Duration.ofSeconds(1)),
   /** 数据质量 */
   DQC("dqc", "", Duration.ofHours(8)),
-  /** 数据质量 规则 */
-  DQC_RULE("dqc:rule", "", Duration.ofHours(12)),
-  CACHE_NAME_DAS_QUERY("dataservice:query", "", Duration.ofMinutes(10)),
-  /** 数据查询服务===>后台查询多条记录 */
-  CACHE_NAME_DAS_QUERY_LIST("dataservice:query:list", CacheManagerEnum.HTTP_DATA_PARAM_MODEL_TO_STRING, Duration.ofMinutes(15)),
-  /** 数据查询服务===>后台查询单记录 */
-  CACHE_NAME_DAS_QUERY_ONE("dataservice:query:one", CacheManagerEnum.HTTP_DATA_PARAM_MODEL_TO_STRING, Duration.ofMinutes(15)),
-  /** 数据查询服务 前台rest模块 */
-  CACHE_NAME_DAS_REST("dataservice:rest:bq", CacheManagerEnum.HTTP_DATA_PARAM_MODEL_TO_STRING, Duration.ofSeconds(15)),
-  /** 数据质量 统计 */
-  DQC_STATISTICS("dqc:statistics", "", Duration.ofHours(24)),
   /** 数据标准 */
   DSD("dsd", "", Duration.ofHours(8)),
   /** 元数据 */
@@ -32,6 +21,19 @@ public enum CacheManagerEnum {
   DATA_MODEL("datamodel", "", Duration.ofHours(8)),
   /** 数据服务 */
   DATA_SERVICE("dataservice", "", Duration.ofHours(8)),
+
+  CACHE_NAME_DAS_QUERY("dataservice:query", "", Duration.ofMinutes(10)),
+  /** 数据查询服务===>后台查询多条记录 */
+  CACHE_NAME_DAS_QUERY_LIST("dataservice:query:list", CacheManagerEnum.HTTP_DATA_PARAM_MODEL_TO_STRING, Duration.ofMinutes(15)),
+  /** 数据查询服务===>后台查询单记录 */
+  CACHE_NAME_DAS_QUERY_ONE("dataservice:query:one", CacheManagerEnum.HTTP_DATA_PARAM_MODEL_TO_STRING, Duration.ofMinutes(15)),
+  /** 数据查询服务 前台rest模块 */
+  CACHE_NAME_DAS_REST("dataservice:rest:bq", CacheManagerEnum.HTTP_DATA_PARAM_MODEL_TO_STRING, Duration.ofSeconds(15)),
+  /** 数据质量 规则 */
+  DQC_RULE("dqc:rule", "", Duration.ofHours(12)),
+  /** 数据质量 统计 */
+  DQC_STATISTICS("dqc:statistics", "", Duration.ofHours(24)),
+
   ;
 
   public static final String HTTP_DATA_PARAM_MODEL_TO_STRING = "#httpDataParamModel.toString()";
